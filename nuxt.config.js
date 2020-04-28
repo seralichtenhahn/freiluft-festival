9f20b93f4979d0042da03bddbbfe63203501f4e8
seralichtenhahn <seralichtenhahn@gmail.com>
2020-04-28 11:08:39 +0200

    lint all files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,25 +1,27 @@
+require("dotenv").config()
+
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
@@ -32,19 +34,38 @@ export default {
    ** Nuxt.js dev-modules
    */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    "@nuxtjs/eslint-module",
+    "@nuxtjs/tailwindcss",
+    "@wearewondrous/nuxt-storyblok-router"
   ],
   /*
    ** Nuxt.js modules
    */
-  modules: [
-    '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
-  ],
+  modules: ["@nuxtjs/pwa", "@nuxtjs/dotenv", "@nuxtjs/apollo"],
+
+  /*
+   * Apollo settings
+   */
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: "https://gapi.storyblok.com/v1/api",
+        httpLinkOptions: {
+          headers: {
+            token: process.env.STORYBLOK_TOKEN,
+            version: process.env.STORYBLOK_VERSION
+          }
+        }
+      }
+    }
+  },
+  /*
+   * Apollo settings
+   */
+  storyblokRouter: {
+    accessToken: process.env.STORYBLOK_TOKEN,
+    version: process.env.STORYBLOK_VERSION
+  },
   /*
    ** Build configuration
    */
@@ -52,6 +73,19 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {},
+    babel: {
+      presets({ isServer }) {
+        return [
+          [
+            require.resolve("@nuxt/babel-preset-app"),
+            {
+              buildTarget: isServer ? "server" : "client",
+              corejs: { version: 3 }
+            }
+          ]
+        ]
+      }
+    }
   }
 }

ddde7ceaa5355e26c0335e7f92e2dafff5c731cf
seralichtenhahn <seralichtenhahn@gmail.com>
2020-05-04 22:22:06 +0200

    add base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme: {},
+  theme: {
+    fontFamily: {
+      headline: ["Montserrat", "sans-serif"],
+      body: ["Raleway", "Roboto", "Helvetica", "Arial", "sans-serif"]
+    },
+    container: {
+      padding: "2rem"
+    },
+    extend: {
+      colors: {
+        primary: "#8BD2F5",
+        secondary: "#F7AD54",
+        headline: "#203B52",
+        body: "#303138",
+        cyan: {
+          default: "#00F2FE",
+          darker: "#16E0FE"
+        }
+      },
+      fontSize: {
+        "7xl": "5rem",
+        "9xl": "6rem",
+        "12xl": "8rem"
+      }
+    }
+  },
   variants: {},
   plugins: []
 }

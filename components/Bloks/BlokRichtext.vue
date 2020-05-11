e978fc7ebac628db0272c4193feb816fee2de396
seralichtenhahn <seralichtenhahn@gmail.com>
2020-05-11 22:50:38 +0200

    update richt text style

---
+++ components/Bloks/BlokRichtext.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-editable="blok" class="container mx-auto mb-8 md:mb-48">
+    <BaseRichText :content="blok.text" class="w-full" />
+  </div>
+</template>
+
+<script>
+import BaseRichText from "@/components/Base/BaseRichText"
+
+export default {
+  components: {
+    BaseRichText
+  },
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>

fb1ca080e7cf26e57f0a83db3bb10a980b52a5a0
seralichtenhahn <seralichtenhahn@gmail.com>
2020-05-11 13:36:56 +0200

    add blok sponsors

---
+++ components/Bloks/BlokSponsors.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-editable="blok">
+    <BlokTitleSection
+      v-if="blok.title_section.length"
+      :blok="blok.title_section[0]"
+    />
+    <div class="container mx-auto overflow-x-hidden">
+      <ul class="flex flex-wrap -mx-8 md:-mx-16">
+        <li
+          v-for="sponsor in blok.sponsors"
+          :key="sponsor._uid"
+          class="w-1/2 px-8 mb-8 md:w-1/4 md:px-16 md:mb-16"
+        >
+          <Sponsor :blok="sponsor" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sponsor from "@/components/Sponsor/Sponsor"
+
+export default {
+  components: {
+    Sponsor
+  },
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ components/Sponsor/Sponsor.vue
@@ -0,0 +1,37 @@
+<template>
+  <a
+    v-if="blok.link.url"
+    rel="noopener"
+    :href="compileExternalLink(blok.link.url)"
+    target="_blank"
+  >
+    <BaseImage :image="blok.image[0]" :options="options" />
+  </a>
+  <BaseImage v-else :image="blok.image[0]" :options="options" />
+</template>
+
+<script>
+import links from "@/mixins/links"
+import BaseImage from "@/components/Base/BaseImage"
+
+export default {
+  components: {
+    BaseImage
+  },
+  mixins: [links],
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      options: [
+        { type: "image/webp", media: false, width: "240" },
+        { type: false, media: false, width: "240" }
+      ]
+    }
+  }
+}
+</script>

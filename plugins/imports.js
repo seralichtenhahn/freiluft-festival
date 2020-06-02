103cf983ac90828b3bb9299455f2e0d18615c3e3
seralichtenhahn <seralichtenhahn@gmail.com>
2020-06-02 12:29:04 +0200

    remove vue-lazyload

--- plugins/imports.js
+++ plugins/imports.js
@@ -1,12 +1,4 @@
 import Vue from "vue"
-import VueLazyload from "vue-lazyload"
 import StoryblokVue from "storyblok-vue"
 
 Vue.use(StoryblokVue)
-Vue.use(VueLazyload, {
-  lazyComponent: true,
-  preLoad: 1.3,
-  error: "dist/error.png",
-  loading: "dist/loading.gif",
-  attempt: 1
-})

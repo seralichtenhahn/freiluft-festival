d74d8e761692e320880a1de6fd8aa14c26a6980e
seralichtenhahn <seralichtenhahn@gmail.com>
2020-06-02 13:34:31 +0200

    remove favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,7 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { name: "theme-color", content: "#8BD2F5" }
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    ]
   },
   /*
    ** ENV Varables

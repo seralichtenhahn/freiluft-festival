be847fba78677c819ea2220a4d383fca244681a2
seralichtenhahn <seralichtenhahn@gmail.com>
2020-06-08 14:11:32 +0200

    add google adsense script

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,13 @@ export default {
     link: [
       { rel: "shortcut icon", href: "/favicon.ico", type: "image/x-icon" },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" }
+    ],
+    script: [
+      {
+        "data-ad-client": "ca-pub-7071258324219296",
+        async: true,
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      }
     ]
   },
   /*

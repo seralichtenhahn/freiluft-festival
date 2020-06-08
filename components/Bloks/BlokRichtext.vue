ac628544ab02d9d9e2de54ef523a7b84d00854fa
seralichtenhahn <seralichtenhahn@gmail.com>
2020-06-08 13:21:46 +0200

    update blok richtext

--- components/Bloks/BlokRichtext.vue
+++ components/Bloks/BlokRichtext.vue
@@ -1,6 +1,10 @@
 <template>
-  <div v-editable="blok" class="container mx-auto mb-8 md:mb-24">
-    <BaseRichText :content="blok.text" class="w-full" />
+  <div
+    v-editable="blok"
+    class="container mx-auto mb-8 md:mb-24"
+    :class="[smallerContainerClass]"
+  >
+    <BaseRichText :content="blok.text" class="w-full mx-auto" />
   </div>
 </template>
 
@@ -15,6 +19,15 @@ export default {
     blok: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    smallerContainerClass() {
+      if (!this.blok.smaller_container) {
+        return ""
+      }
+
+      return "md:px-24 lg:px-56"
     }
   }
 }

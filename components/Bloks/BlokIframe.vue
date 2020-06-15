112549614947078c46b283b65500d43984606d64
seralichtenhahn <seralichtenhahn@gmail.com>
2020-06-15 22:32:49 +0200

    add blok-iframe

---
+++ components/Bloks/BlokIframe.vue
@@ -0,0 +1,28 @@
+<template>
+  <BaseMedia v-editable="blok" :container="blok.container" :size="blok.size">
+    <iframe
+      :src="blok.src"
+      :height="blok.height"
+      class="w-full"
+      frameborder="0"
+      allowtransparency="true"
+      :allow="blok.allow"
+    ></iframe>
+  </BaseMedia>
+</template>
+
+<script>
+import BaseMedia from "@/components/Base/BaseMedia"
+
+export default {
+  components: {
+    BaseMedia
+  },
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
